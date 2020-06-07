bf493ca7096b5b0b9ef9590624295b600f3bae91
jun <jun@gmail.com>
2020-06-07 17:07:07 +1200

    added main css file

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,26 +6,36 @@
  */
 
 import React from "react"
-import Header from './header';
-import {Helmet} from 'react-helmet';
-
+import Header from "./header"
+import { Helmet } from "react-helmet"
 
 const Layout = ({ children }) => {
-  
-
   return (
     <>
       <Helmet>
         <title>Queen Tourism</title>
         <meta name="description" content="Queen Tourism Description" />
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Noto+Serif+SC:wght@700&family=Overpass:wght@300;700&family=Strait&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300;500;700&family=ZCOOL+XiaoWei&display=swap"
+          rel="stylesheet"
+        ></link>
       </Helmet>
       <Header />
       {children}
     </>
   )
 }
-
 
 export default Layout
