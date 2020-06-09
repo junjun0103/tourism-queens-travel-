9cf00f6a979f4f5c1b592a8eda09b8790957eb63
jun <jun@gmail.com>
2020-06-09 12:29:03 +1200

    review page preview css added

--- src/components/iterinaryEn/listIterinary.js
+++ src/components/iterinaryEn/listIterinary.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
-import UseItinerary from "../../hooks/useItinerary";
-import PreviewIterinary from './previewIterinary';
-=======
 import React, { useState, useEffect } from "react"
 import UseItinerary from "../../hooks/useItinerary"
 import PreviewIterinary from "./previewIterinary"
 import Title from "../title"
 import SubTitle from "../subtitle"
->>>>>>> 3e685a45c924cf0e0e646c7c4dc6dbd20154fbcb
 
 const ListIterinary = () => {
   //bring data from hook
