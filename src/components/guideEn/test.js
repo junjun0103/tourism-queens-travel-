81f844399afa9cba147197dc9700a809dfa099af
CIAO <ariaw@hotmail.com>
2020-07-30 21:45:11 +1200

    test4

--- src/components/guideEn/test.js
+++ src/components/guideEn/test.js
@@ -2,15 +2,37 @@ import React from 'react';
 import Layaout from '../ui/layout';
 import {graphql} from 'gatsby';
 
+export const query = graphql`
+    query($slug:String!){
+        allStrapiTouristInformation(filter:{slug:{eq:$slug}}){
+            nodes {
+                slug
+                title_cn
+                title_en
+                notice_cn
+                notice_en
+                information_cn {
+                  content
+                  id
+                  subject
+                }
+                information_en {
+                  content
+                  id
+                  subject
+                }
+            }
+        }
+    }
+    `;
 
-
-const OneFeedBack = () => {
+const OneFeedBack = ({data:{allStrapiTouristInformation:{nodes}}}) => {
     //restructure
-    
+    const {title_en} = nodes[0];
 
     return (        
         <Layaout>
-           <h1>Hola</h1>
+           <h1>{title_en}</h1>
         </Layaout>        
      );
 }
