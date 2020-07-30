a9c6ffb29e3cea0e3871eaf7f6305c164147f8cf
CIAO <ariaw@hotmail.com>
2020-07-30 17:46:02 +1200

    test2

--- gatsby-node.js
+++ gatsby-node.js
@@ -96,9 +96,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     informations.forEach(information => {
       actions.createPage({
         path: `/touristInformation/${information.slug}/`,
-        component: require.resolve(
-          "./src/components/touristinfo/oneTouristInfo.js"
-        ),
+        component: require.resolve("./src/components/touristinfo/oneTouristInfo.js"),
         context: {
           slug: information.slug,
         },
